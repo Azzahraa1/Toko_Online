5143997976346c194e87f302e3176f3c5b9151b2
Azzahra1 <fatimahazzahra4121@gmail.com>
2023-08-23 14:25:25 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<?php 
+    include "header.php";
+?>
+<h2 style = "text-align:center" >Selamat datang <?=$_SESSION['nama_user']?> Happy Shopping.</h2>
+<hr/>
+<img src = "tokoonline.jpg" alt="logo" align="center">
+
+<?php
+    include "footer.php";
+?>
+
+</body>
+</html>
